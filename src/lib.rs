10c10954080428ab4cc0f147c342474b0d2c1775
prepodobnuy <zvfvbycsy6@gmail.com>
2025-02-23 20:19:40 +0300

    im crying

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,3 @@
 pub mod layout;
 pub mod style;
 pub mod backends;
-
-pub use layout::{Container, Rll};
-pub use style::*;
-pub use backends::crossterm::colors;
-pub use backends::crossterm::backend::Backend;
-pub use backends::crossterm::drawer::Drawer;
-pub use backends::crossterm::listener::Listener;
